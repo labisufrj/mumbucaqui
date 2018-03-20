7389156f294607f68ae4effbb44a5d383e3e1b5b
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-03-20 16:28:27 -0300

    Adicionado script de inserção de múltiplos marcadores no mapa

---
+++ scripts/maricainfo.js
@@ -0,0 +1,60 @@
+let map;
+
+function init() {
+    map = new google.maps.Map(getIdContent("map"), {
+        zoom: 6,
+        center: new google.maps.LatLng(-22.5206801,-43.1674506),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+}
+
+function addCep() {
+    let ceps = getCEPList();
+    let lat, lng;
+    let geocoder = new google.maps.Geocoder();
+    
+    ceps.forEach(function(cep, index){
+        geocoder.geocode( { 'address': cep}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                lat = getLat();
+                lng = getLng();
+                addMarker(lat, lng);
+            }
+        
+            else {
+                alert("CEP " + (index + 1) + " Inválido!");
+                return 0
+            }
+        });
+
+    });
+}
+ 
+function addMarker(lat, lng) {
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map
+    });
+}
+
+function getIdContent(id) {
+    let element = document.getElementById(id);
+    return element
+}
+
+function getCEPList() {
+    let input = getIdContent("cep").value;
+    ceps = input.split(", ");
+    console.log(ceps);
+    return ceps
+}
+
+function getLat() {
+    let lat = results[0].geometry.location.lat();
+    return lat
+}
+
+function getLng() {
+    let lng = results[0].geometry.location.lng();
+    return lng
+}
