c312d1ae0c0054cb77855a53e780e8a5780c3296
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-03-22 07:53:14 -0300

    Adicionando código comentado para possível trabalho com Cross Origin Resource Sharing

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+/*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
+
 app.get("/", function(req, res){
 	res.render("home.ejs");
 });
